09f8a71bf9674f0834f9f4c411e9e9edd91dfb64
Thiago Augusto de Oliveira <63484436+professorThiago@users.noreply.github.com>
2024-10-08 14:24:39 -0300

    tentando recuperar

--- scr/services/api.js
+++
@@ -1,7 +0,0 @@
-import axios from 'axios';
-
-const API_URL = process.env.REACT_APP_API_URL;
-
-export const connectToBroker = async (data) => {
-  return axios.post(`${API_URL}/connect`, data);
-};
